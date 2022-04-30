da4e40981a3ebe9a58deb6cca7e7f2c2ad0bef28
Fernando J Monteiro <ferjmonteiro@gmail.com>
2022-04-30 15:35:32 -0300

    finalizando requisito 7

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
-function limpaQuadro() {
-    let limpa = document.getElementsByClassName('pixel')
-    limpa = background-color: rgb(255, 255, 255)
-}   
-limpaQuadro()
+function selecionar() {
+    const p1 = document.getElementById("p1")
+    const p2 = document.getElementById("p2")
+    const p3 = document.getElementById("p3")
+    const p4 = document.getElementById("p4")
+
+    p1.addEventListener("click", function () {
+        p2.classList.remove("selected");
+        p3.classList.remove("selected");
+        p4.classList.remove("selected");
+        p1.classList.add("selected");
+    })
+    p2.addEventListener("click", function () {
+        p4.classList.remove("selected");
+        p1.classList.remove("selected");
+        p3.classList.remove("selected");
+        p2.classList.add("selected");
+    })
+    p3.addEventListener("click", function () {
+        p2.classList.remove("selected");
+        p4.classList.remove("selected");
+        p1.classList.remove("selected");
+        p3.classList.add("selected");
+    })
+    p4.addEventListener("click", function () {
+        p2.classList.remove("selected");
+        p3.classList.remove("selected");
+        p1.classList.remove("selected");
+        p4.classList.add("selected");
+    })
+}
+
+selecionar()
+
+
+
+
+
+
+
+
